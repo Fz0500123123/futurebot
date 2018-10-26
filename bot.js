7034e6a5e03b6a4a4c9aaf1490795577ec16ba97
Fz0500123123 <41424919+Fz0500123123@users.noreply.github.com>
2018-10-26 13:24:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,24 +55,4 @@ client.on('message',message =>{
     }
   });
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix+say)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
-
-message.channel.send(args.join("  "))
-    message.delete();
-  }
-
-
-
-});
-  
 client.login(process.env.BOT_TOKEN);
