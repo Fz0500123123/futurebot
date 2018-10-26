d6ea9918a86d93c4906716ee3258f7dabfd61475
Fz0500123123 <41424919+Fz0500123123@users.noreply.github.com>
2018-10-26 15:42:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,7 @@ client.on('message',message =>{
     }
   });
 
+var prefix = "."
 client.on('message', message => {
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
@@ -92,6 +93,24 @@ client.on('message', message => {
     embed : banembed
   })
 }
+});
+  
+client.on('message' , message => {
+    let user = message.mentions.users.first()|| client.users.get(message.content.split(' ')[1])
+    if(message.content.startsWith(prefix + 'unban')) {
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك رتبة`**');
+        if(!user) return  message.channel.send(`Do this ${prefix} <@ID user> \n or \n ${prefix}unban ID user`);
+        message.guild.unban(user);
+        message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
+        var embed = new Discord.RichEmbed()
+        .setThumbnail(message.author.avatarURl)
+        .setColor("RANDOM")
+        .setTitle('**●Unban** !')
+        .addField('**●User Unban :** ', `${user}` , true)
+        .addField('**●By :**' ,       ` <@${message.author.id}> ` , true)
+        .setAuthor(message.guild.name)
+        message.channel.sendEmbed(embed)
+    }
 });
   
 client.login(process.env.BOT_TOKEN);
