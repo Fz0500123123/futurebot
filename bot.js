760218e7ca46c03148e9649b786229ce9d7bf8f4
Fz0500123123 <41424919+Fz0500123123@users.noreply.github.com>
2018-11-17 11:25:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -512,6 +512,16 @@ if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return 
     }
 });
 		 
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date < 7) {
+    member.ban("Member account age is lower than 7 days.")
+  }
+});
+
 ////////////////////////////////////////////////
 
 client.on('message', function(message) {
